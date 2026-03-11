fbc19959a02a7dd89e00ec3684c2b0382a2962e7
Mikhail Baravoi <s32798@pjwstk.edu.pl>
2026-03-11 10:49:00 +0100

    more features

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Press nothing to exit");
+
+var a = 1;
+var b = 2;
+var c = a + b;
+Console.WriteLine(c);;
