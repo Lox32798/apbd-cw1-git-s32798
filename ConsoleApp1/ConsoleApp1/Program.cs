9b4f0ec8f285747507123ecfd6596fb622b6034f
Mikhail Baravoi <s32798@pjwstk.edu.pl>
2026-03-11 10:47:17 +0100

    more features

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Press nothing to exit");
+Console.WriteLine("Feature1");
+Console.WriteLine("Feature2");
